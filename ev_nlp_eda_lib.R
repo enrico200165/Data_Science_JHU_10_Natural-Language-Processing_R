d2b4bfca5896f56736a3dd4a1b50890c33908375
enrico200165 <enrico200165@tiscali.it>
2018-07-04 11:42:40 +0200

    completing and tidying up code of EDA library file

--- ev_nlp_eda_lib.R
+++ ev_nlp_eda_lib.R
@@ -1,6 +1,7 @@
 require(dplyr)
 require(stringr)
 
+source("01_globals.R")
 
 #---------------------------------------------------------------------
   wcForFile <- function(fdir,fname) 
@@ -22,10 +23,11 @@ require(stringr)
   args <- paste(args,"--max-line-length")
   args <- paste(args, file_path)
   out <- system2(command = command
-                 ,args = args,
-                 stdout = TRUE, stderr = "", stdin = "", input = NULL
-                 #,env = character(), wait = TRUE
-                 #,minimized = FALSE, invisible = TRUE, timeout = 0
+   ,args = args, stdout = TRUE
+   ,stderr = "" ,stdin = "" 
+   ,input = NULL
+  #,env = character(), wait = TRUE
+  #,minimized = FALSE, invisible = TRUE, timeout = 0
   )
   
   # print(out)
@@ -43,8 +45,8 @@ require(stringr)
     ,stringsAsFactors=FALSE
   )
   names(wcDfRow) <- c(TXT_FNAME,TXT_BYTES,TXT_NCHAR,TXT_NTOKENS,TXT_NNLINES,"max_line_len")
-  
   # print(wcDfRow)
+
   wcDfRow
 }
 
@@ -68,13 +70,13 @@ require(stringr)
     }
     args <- paste(args,file_path)
     out <- system2(command = command
-                   ,args = args,
-                   stdout = TRUE, stderr = "", stdin = "", input = NULL
-                   #,env = character(), wait = TRUE
-                   #,minimized = FALSE, invisible = TRUE, timeout = 0
+     ,args = args ,stdout = TRUE, stderr = "" ,stdin = ""
+     ,input = NULL
+    #,env = character(), wait = TRUE
+    #,minimized = FALSE, invisible = TRUE, timeout = 0
     )
-    
     # print(out)
+
     spl <- strsplit(trimws(out)," +")
     spl <- spl[[1]] # extract from list
     size = str_extract(spl[5], "[0-9]+")
@@ -86,8 +88,9 @@ require(stringr)
       ,TXT_SIZE_U = units
       ,stringsAsFactors=FALSE
     )
-    # print(wcDfRow)
     names(dfRow) <- c(TXT_FNAME,TXT_SIZE,TXT_SIZE_U)
+    # print(dfRow)
+
     dfRow
   }
   
@@ -107,6 +110,7 @@ require(stringr)
   wcDf <- bind_rows(myrows)
 }
 
+  
 # --------------------------------------------------------------------
   physicalAnalysis <- function()
 #---------------------------------------------------------------------
@@ -144,72 +148,115 @@ require(stringr)
     
     mydf
 }
-  
-  
 
 
-# ====================================================================
-#                              TESTS
-# ====================================================================
-
-
-testIt <- function(){
-
-  source("01_globals.R")
-  lsForFile_bound <- function(x) lsForFile(x,data_dir_corpus_in, "--block-size=M") 
-  linux_wc_bound <- function(x) wcForFile(data_dir_corpus_in,x)
-  
-  if (readIfEmpty(linux_wc_bound)) {
-  } else {
-    print("NO, I could NOT read it")
-    wc_df <- getWCInfo(data_dir_corpus_in,"*.txt",linux_wc_bound)
-    print(wc_df)
-    serializeIfNeeded(linux_wc_bound,FALSE)  
-  }
-  
+# --------------------------------------------------------------------
+  basicPlot <- function(dfPar,xPar,yPar,fillPar)  
+# --------------------------------------------------------------------
+{
+  p <- ggplot(data=dfPar, aes_string(x = xPar, y = yPar)) 
+  p <- p + geom_bar(stat="identity"
+                      ,aes_string(fill = fillPar), position = "dodge")
+  # p <- p + facet_grid(~language)
+  p <- p + theme(axis.text.x = element_text(angle = 90, hjust = 1))
 }
+  
+  
 
-# if(T) testIt()
-# 
+# ====================================================================
+#                   Tests (quick verification)
+# ====================================================================
+  
+# -------------------------------------------------------------------
+  test_basicPlot <- function(mydf) 
+# -------------------------------------------------------------------
+{
+    if (!(missing(mydf)) && !is.null(mydf)) {
+      print(basicPlot(mydf ,TXT_TYP,TXT_NTOKENS ,TXT_LNG))
+      print(basicPlot(mydf ,TXT_LNG ,TXT_NTOKENS ,TXT_TYP))
+    } else {
+      print("test_basicPlot() did not receive a usable df")
+    }
+}
+  
 
-
-
-#if(T) {
-  source("01_globals.R")
-
-  require(ggplot2)
-
+# -------------------------------------------------------------------
+  test_physicalAnalysis <- function() 
+# -------------------------------------------------------------------
+{
   if (readIfEmpty(phys_df)) {
   } else {
     print("NO, I could NOT read it")
     phys_df <- physicalAnalysis()
     serializeIfNeeded(phys_df,FALSE)  
   }
-
+  
   p <- ggplot(data=phys_df, aes(x = type, y = n_token)) 
   p <- p + geom_bar(stat="identity"
-                    ,aes(fill = language), position = "dodge"
-                    )  
+                    ,aes(fill = language), position = "dodge")  
   # p <- p + facet_grid(~language)
   p <- p + theme(axis.text.x = element_text(angle = 90, hjust = 1))
   print(p)
-
   
-  
-    
   print(str(phys_df))
   print(phys_df)
-#}
+}
 
-basicPlot <- function(xPar,yPar,fillPar)  {
-  p <- ggplot(data=phys_df, aes_string(x = xPar, y = yPar)) 
-  p <- p + geom_bar(stat="identity"
-                    ,aes_string(fill = fillPar), position = "dodge")
-  # p <- p + facet_grid(~language)
-  p <- p + theme(axis.text.x = element_text(angle = 90, hjust = 1))
-  print(p)
+  
+# -------------------------------------------------------------------
+  test_readIfEmpty_serializeIfNeeded <- function()
+# -------------------------------------------------------------------
+{
+    
+  if (readIfEmpty(linux_wc)) {
+  } else {
+    print("NO, I could NOT read serialization")
+    linux_wc <- function(x) wcForFile(data_dir_corpus_in,x)
+    wc_df <- getWCInfo(data_dir_corpus_in,"*.txt",linux_wc)
+    # print(wc_df)
+    serializeIfNeeded(linux_wc,FALSE)  
+  }
+  linux_wc <- NULL
+  if (readIfEmpty(linux_wc)) {
+  } else {
+    print("NO, I could NOT read serialization")
+    linux_wc <- function(x) wcForFile(data_dir_corpus_in,x)
+    wc_df <- getWCInfo(data_dir_corpus_in,"*.txt",linux_wc)
+    # print(wc_df)
+    serializeIfNeeded(linux_wc,FALSE)  
+  }
+
+  if (readIfEmpty(linux_ls)) {
+  } else {
+    print("NO, I could NOT read it")
+    linux_ls <- function(x) lsForFile(x,data_dir_corpus_in, "--block-size=M") 
+    wc_df <- getWCInfo(data_dir_corpus_in,"*.txt",linux_ls)
+    print(wc_df)
+    serializeIfNeeded(linux_ls,FALSE)  
+  }
+  if (readIfEmpty(linux_ls)) {
+  } else {
+    print("NO, I could NOT read it")
+    linux_ls <- function(x) lsForFile(x,data_dir_corpus_in, "--block-size=M") 
+    wc_df <- getWCInfo(data_dir_corpus_in,"*.txt",linux_ls)
+    print(wc_df)
+    serializeIfNeeded(linux_ls,FALSE)  
+  }
   
 }
-basicPlot(TXT_TYP,TXT_NTOKENS,TXT_LNG)
-basicPlot(TXT_LNG,TXT_NTOKENS,TXT_TYP)
+
+  
+
+# -------------------------------------------------------------------
+  test_ev_nlp_eda_lib.R <- function()
+# -------------------------------------------------------------------
+{
+  test_readIfEmpty_serializeIfNeeded()
+  
+  test_physicalAnalysis()
+  
+  test_basicPlot(physicalAnalysis())
+}
+
+# test_ev_nlp_eda_lib.R()
 
