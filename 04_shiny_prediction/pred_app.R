ebfb64ea0cd4c8abce5e7a310b15f1ce54bf2db9
enrico200165 <enrico200165@gmail.com>
2018-07-20 10:47:54 +0200

    started experimenting with shiny event handling

--- 04_shiny_prediction/pred_app.R
+++ 04_shiny_prediction/pred_app.R
@@ -1,6 +1,6 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
+# resources 
+# VITAL: https://shiny.rstudio.com/articles/js-send-message.html in particular
+# https://stackoverflow.com/questions/47215230/listen-to-button-events-with-shiny-oninputchange-r-shiny
 #
 # Find out more about building applications with Shiny here:
 #
@@ -8,13 +8,24 @@
 #
 
 library(shiny)
+library(shinyjs)
+require(ggplot2)
+
+
+# ------------------------ IDs -------------------------
+
+EVT_KEY_PRESS <- "keypress"
+EVT_KEY_DOWN <- "keydown"
+EVT_KEY_UP <- "keyup"
+
+LOG_TEXT <- "traceOut"
+
+TXT_IN_ID <- "txti"
 
 
 #####################################################################
 #                 BACKEND
 #####################################################################
-curdf <- data.frame(x=1:10)
-values <- "dummy"
 
 
 utlCmdMenu <- list('On Data Frame' = c("names", "nrow","ncol"),
@@ -26,6 +37,7 @@ plotParamConsts <- list(
   regrPlotSmooth = c("None","Lm","Loess")
   
 )
+
 setClass("plotParsClass",
          slots = c(regrSmoot = "character"
                    , pointSize = "numeric")
@@ -33,17 +45,15 @@ setClass("plotParsClass",
 plotPars = new("plotParsClass")
 
 
-initBE <- function() {
-  data("mtcars")
-  values <<- reactiveValues()
-  values$msg <<- "reactive value message"
-  
-  curdf <<- mtcars
-}
+# initBE <- function() {
+#   data("mtcars")
+#   values <<- reactiveValues()
+#   values$msg <<- "reactive value message"
+#   
+#   curdf <<- mtcars
+# }
 
-setMsg <- function(m) { 
-  values$msg <<- m; 
-}
+setMsg <- function(m) { values$msg <<- m; }
 
 plotRegression <- function(xpar, ypar,dfra,plotPar) {
   p <- ggplot(data = dfra, aes(x=dfra[[xpar]],y=dfra[[ypar]]))
@@ -59,17 +69,13 @@ plotRegression <- function(xpar, ypar,dfra,plotPar) {
 
 
 performVariableCommand <- function(cmdPar, var) {
-  cmd <- match.fun(cmdPar)
+
   setMsg(paste("function:",cmdPar,"on",var))
-  return(cmd(mtcars[[var]]))
+  match.fun(cmdPar)(mtcars[[var]])
 }
 
 
-performDFCommand <- function(cmdPar, var) {
-  cmd <- match.fun(cmdPar)
-  ret <- cmd(mtcars)
-  ret
-}
+performDFCommand <- function(cmdPar , var) match.fun(cmdPar)(mtcars)
 
 
 
@@ -85,27 +91,20 @@ ui <- fluidPage(
     sidebarLayout(
       sidebarPanel(
         
-        selectInput("yVar", "Choose Y Variable:",
-                    names(mtcars), selected = 1
-        )
+        selectInput("yVar", "Choose Y Variable:", names(mtcars), selected = 1)
         
-        ,selectInput("xVar", "Choose X Variable:",
-                     names(mtcars)
-                     ,selected = names(mtcars)[length(names(mtcars))]
-        )
+        ,selectInput("xVar", "Choose X Variable:", names(mtcars) 
+                     ,selected = names(mtcars)[length(names(mtcars))])
         
-        ,radioButtons("regrLine", "Regression Smoothing",
-                      plotParamConsts$regrPlotSmooth,
-                      selected = "Loess"
-        )
-        ,sliderInput("pointSize",
-                     "Size of points in plot:"
-                     ,min = 1, max = 8,value = 2)
+        ,radioButtons("regrLine", "Regression Smoothing", plotParamConsts$regrPlotSmooth ,selected = "Loess")
+        ,sliderInput("pointSize", "Size of points in plot:" ,min = 1, max = 8,value = 2)
         
         ,hr()
-        ,selectInput("utlCmdId", "Choose a Statistic:",utlCmdMenu,
-                     selected = "median")
+        ,selectInput("utlCmdId", "Choose a Statistic:",utlCmdMenu, selected = "median")
       ) # sidebar panel
+
+      
+      
       # Show a plot of the generated distribution
       ,mainPanel(
         h3(strong("Documentation")
@@ -113,6 +112,20 @@ ui <- fluidPage(
                    ,"here",style="color:blue;"),style="color:red"
         )
         ,hr()
+        
+        ,tags$script('
+        $(document).on("keypress", function (e) {
+          //if(e.which == " ") {
+            Shiny.onInputChange("tasto", e.which);
+            alert("key_press" + e.which)
+          //}
+        });')
+        ,div(style="display: inline-block;vertical-align:top;"
+            ,textInput(TXT_IN_ID, label = h3("Text input"), value = ""))
+        ,div(style="display: inline-block;vertical-align:top;",
+            textOutput(LOG_TEXT))
+      
+        
         ,plotOutput("regrPlot")
         ,h3("Stat functions output",style="color:blue")
         ,textOutput("utlCmdOut")
@@ -122,7 +135,7 @@ ui <- fluidPage(
         ,textOutput("globalStatus")
         ,hr()
         ,h3("Debug messages",style="color:blue")
-        ,textOutput("traceOut")
+
       )
     )
   )
@@ -132,6 +145,24 @@ ui <- fluidPage(
 #                                 SERVER
 ########################################################################
 server <- function(input, output) {
+  
+  print("Server started")
+  
+  # ---- EVENTS
+  do_test <- function() { 
+    print(rep("#",50))
+    output$globalStatus <<- "premuto"; 
+    output$globalStatus <- "premuto"; 
+    output$traceOut <- "premuto" 
+  }
+  observeEvent(input$tasto, {
+    #output$globalStatus <- "premuto"
+    print("YEAH")
+  })
+  onevent(EVT_KEY_PRESS, "textSample",do_test )
+  #onevent("mouseleave", "disappear", show("text"))
+  onevent(EVT_KEY_UP, "textSample", do_test)
+  onevent(EVT_KEY_DOWN, "textSample", do_test)
   
   output$globalStatus <- renderText({
     status <- paste("y var:",input$yVar
