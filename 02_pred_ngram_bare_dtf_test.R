baf8b5a9c3df328e8f77d317f0bd605f81c749fe
enrico200165 <enrico200165@tiscali.it>
2018-08-05 20:29:36 +0200

    completing ngrams calculation, still something to do (remove useless columns like group)

--- 02_pred_ngram_bare_dtf_test.R
+++ 02_pred_ngram_bare_dtf_test.R
@@ -1,5 +1,5 @@
 
-# rm(list=ls(all=TRUE))
+rm(list=ls(all=TRUE))
 
 source("02_pred_ngram_bare_dtf.R")
 
@@ -13,8 +13,6 @@ use_full_corpus(T,ngram_bare_re_init)
 
 
 ret <- produce_ngram_bare_dtf()
-print(str(ret))
-
 
 dtf_info(dtf_1gram_sep)
 dtf_info(dtf_2gram_sep)
