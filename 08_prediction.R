2d92965b20ce4e80b59d72f76cde0c63a14686a7
enrico200165 <enrico200165@gmail.com>
2019-06-20 21:13:58 +0200

    prima semplice versione dell'algoritmo di predizione

--- 08_prediction.R
+++ 08_prediction.R
@@ -21,16 +21,62 @@ pred_init()
 
 pred_successors <- function(predecessors) {
   
+  if (any(is.null(predecessors), length(predecessors) == 0)) {
+    successors <- ngrams_freqs[[1]]
+    return(successors)
+  }
+    
   n_predecessors <- length(predecessors)
   pred_ngram <<- ngrams_freqs[[n_predecessors+1]]
   
   pre_list <- as.list(predecessors)
   successors <- pred_ngram[pre_list]
+  has_nas <- any(is.na(successors[[TYPES_COLNAMES[n_predecessors+1]]]))
+  if (any(nrow(successors) == 0, has_nas)) {
+    if (length(predecessors) >= 2) {
+      predecessors <- predecessors[2:length(predecessors)]
+    } else if (length(predecessors) <= 1) {
+      predecessors <- NULL
+    }
+    return(pred_successors(predecessors))
+  } 
   
   # print(successors)
   
   successors
 }
+
+
+ret <- pred_successors(c("asdsa","sss"))
+print(ret)
+
+
+
+ret <- pred_successors(c("sss","asdasda"))
+print(ret)
+
+
+ret <- pred_successors(c("a£$%£%","%&//"))
+print(ret)
+
+
+ret <- pred_successors(c("the","usual"))
+print(ret)
+
+
+ret <- pred_successors(c("a£$%£%","usual"))
+print(ret)
+
+
+ret <- pred_successors(c("a£$%£%","%&//"))
+print(ret)
+
+
+print(pred_successors(NULL))
+
+# to make 2pred fail and fall down to 1 prede
+predecessors2_nosecond <- setdiff(
+  ngrams_freqs[[2]][["primo"]],ngrams_freqs[[3]][["secondo"]])
 
 
 ret <- pred_successors(c("and","the"))
@@ -42,5 +88,9 @@ print(ret)
 ret <- pred_successors(c("you","are"))
 print(ret)
 
+# last predecessor fails
+
+ret <- pred_successors(c("the","usual"))
+print(ret)
 
 
